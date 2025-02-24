ea8c909ac1fae70cf36543edd72492426bcdddd8
Santiago Firpo <santiagoifirpo4@gmail.com>
2025-02-24 19:58:52 -0300

    First commit

---
+++ src/main/java/org/firpy/keycloakwrapper/KeycloakWrapperApplication.java
@@ -0,0 +1,15 @@
+package org.firpy.keycloakwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakWrapperApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(KeycloakWrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/firpy/keycloakwrapper/KeycloakWrapperApplicationTests.java
@@ -0,0 +1,15 @@
+package org.firpy.keycloakwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakWrapperApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
