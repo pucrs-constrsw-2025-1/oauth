e9860666094abf981609e9b060584df1532ce1ff
Santiago Firpo <s.firpo@edu.pucrs.br>
2025-02-25 23:58:04 -0300

    Added dotenv for local testing

--- server/src/main/java/org/firpy/keycloakwrapper/KeycloakWrapperApplication.java
+++ server/src/main/java/org/firpy/keycloakwrapper/KeycloakWrapperApplication.java
@@ -1,7 +1,9 @@
 package org.firpy.keycloakwrapper;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -9,6 +11,12 @@ public class KeycloakWrapperApplication
 {
     public static void main(String[] args)
     {
+        Dotenv dotenv = Dotenv.configure()
+                              .ignoreIfMissing()
+                              .load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(KeycloakWrapperApplication.class, args);
     }
 }
