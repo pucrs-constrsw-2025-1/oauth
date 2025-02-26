c939fc86282b54311fc4b83d7b20ebf731070345
Santiago Firpo <s.firpo@edu.pucrs.br>
2025-02-25 23:58:35 -0300

    Used client secret to request a token from keycloak

--- server/src/main/java/org/firpy/keycloakwrapper/adapters/login/LoginController.java
+++ server/src/main/java/org/firpy/keycloakwrapper/adapters/login/LoginController.java
@@ -1,5 +1,9 @@
 package org.firpy.keycloakwrapper.adapters.login;
 
+import org.firpy.keycloakwrapper.adapters.login.keycloak_adapter.AccessToken;
+import org.firpy.keycloakwrapper.adapters.login.keycloak_adapter.AccessTokenRequest;
+import org.firpy.keycloakwrapper.adapters.login.keycloak_adapter.KeycloakClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("login")
 public class LoginController
 {
-    /**
+	public LoginController(KeycloakClient keycloakClient)
+	{
+		this.keycloakClient = keycloakClient;
+	}
+
+	/**
      * Consumir a rota POST {{base-keycloak-url}}/auth/realms/{{realm}}/protocol/openid-connect/token da REST API
      * do Keycloak para autenticação de usuário, gerando o access_token e o refresh_token a partir do
      * client_id,
@@ -21,6 +30,28 @@ public class LoginController
     @PostMapping()
     public LoginResponse login(@RequestBody LoginRequest loginRequest)
     {
-        return new LoginResponse("bearer", "token", 3600, "refreshToken", 3600);
+	    AccessTokenRequest request = new AccessTokenRequest
+		(
+			loginRequest.clientId(),
+			clientSecret,
+			loginRequest.username(),
+			loginRequest.password(),
+	"password"
+		);
+
+	    AccessToken accessToken = keycloakClient.getAccessToken(request);
+        return new LoginResponse
+		(
+			accessToken.tokenType(),
+			accessToken.accessToken(),
+			accessToken.expiresIn(),
+			accessToken.refreshToken(),
+			accessToken.refreshExpiresIn()
+		);
     }
+
+    private final KeycloakClient keycloakClient;
+
+	@Value("${keycloak.client-secret}")
+	private String clientSecret;
 }
--- server/src/main/java/org/firpy/keycloakwrapper/adapters/login/keycloak_adapter/AccessToken.java
+++ server/src/main/java/org/firpy/keycloakwrapper/adapters/login/keycloak_adapter/AccessToken.java
@@ -1,5 +1,5 @@
 package org.firpy.keycloakwrapper.adapters.login.keycloak_adapter;
 
-public record AccessToken()
+public record AccessToken(String tokenType, String accessToken, int expiresIn, String refreshToken, int refreshExpiresIn)
 {
 }
