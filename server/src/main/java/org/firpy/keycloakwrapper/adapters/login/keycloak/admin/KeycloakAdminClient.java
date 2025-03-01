ffa8426d286716362bf75e76f79d7f2dc0baa736
LucasAlvso <lucas.soares008@edu.pucrs.br>
2025-03-01 15:44:44 -0300

    Fixed routes

--- server/src/main/java/org/firpy/keycloakwrapper/adapters/login/keycloak/admin/KeycloakAdminClient.java
+++ server/src/main/java/org/firpy/keycloakwrapper/adapters/login/keycloak/admin/KeycloakAdminClient.java
@@ -1,5 +1,6 @@
 package org.firpy.keycloakwrapper.adapters.login.keycloak.admin;
 
+import org.firpy.keycloakwrapper.adapters.login.keycloak.CreateClientRequest;
 import org.firpy.keycloakwrapper.adapters.login.keycloak.auth.KeycloakUser;
 import org.firpy.keycloakwrapper.adapters.users.UpdateUserPasswordRequest;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,21 +10,24 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "keycloak-admin-service", url = "${keycloak.url}")
 public interface KeycloakAdminClient
 {
-	@PostMapping("/auth/admin/realms/${keycloak.realm}/users")
+	@PostMapping("/admin/realms/${keycloak.realm}/users")
 	ResponseEntity<Void> createUser(@RequestHeader("Authorization") String accessToken, KeycloakUser user);
 
-	@GetMapping("/auth/admin/realms/${keycloak.realm}/users")
+	@GetMapping("/admin/realms/${keycloak.realm}/users")
 	KeycloakUser[] getUsers(@RequestHeader("Authorization") String accessToken);
 
-	@GetMapping("/auth/admin/realms/${keycloak.realm}/users/{id}")
+	@GetMapping("/admin/realms/${keycloak.realm}/users/{id}")
 	KeycloakUser getUser(@RequestHeader("Authorization") String accessToken, @PathVariable("id") String id);
 
-	@PutMapping("/auth/admin/realms/${keycloak.realm}/users/{id}")
+	@PutMapping("/admin/realms/${keycloak.realm}/users/{id}")
 	ResponseEntity<Void> updateUser(@RequestHeader("Authorization") String accessToken, @PathVariable("id") String id, KeycloakUser user);
 
-	@PutMapping("auth/admin/realms/${keycloak.realm}/users/{id}/reset-password")
+	@PutMapping("/admin/realms/${keycloak.realm}/users/{id}/reset-password")
 	ResponseEntity<Void> resetPassword(@RequestHeader("Authorization") String accessToken, @PathVariable("id") String id, @RequestBody UpdateUserPasswordRequest request);
 
-	@DeleteMapping("auth/admin/realms/${keycloak.realm}/users/{id}")
+	@DeleteMapping("/admin/realms/${keycloak.realm}/users/{id}")
 	ResponseEntity<Void> deleteUser(@RequestHeader("Authorization") String accessToken, @PathVariable("id") String id);
+
+	@PostMapping("/admin/realms/${keycloak.realm}/clients")
+	ResponseEntity<Void> createClient(@RequestHeader("Authorization") String accessToken, @RequestBody CreateClientRequest request);
 }
