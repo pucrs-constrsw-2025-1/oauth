19ea39b443133cf3b73d26b6287242f52aad8b21
LucasAlvso <lucas.soares008@edu.pucrs.br>
2025-03-03 18:51:07 -0300

    Added scheduling for client creation

--- server/src/main/java/org/firpy/keycloakwrapper/setup/ClientConfig.java
+++ server/src/main/java/org/firpy/keycloakwrapper/setup/ClientConfig.java
@@ -11,11 +11,14 @@ import org.firpy.keycloakwrapper.adapters.users.CredentialRequest;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 @Configuration
 @Slf4j
+@EnableScheduling
 public class ClientConfig
 {
     public ClientConfig(KeycloakAuthClient keycloakAuthClient, KeycloakAdminClient keycloakClient)
@@ -24,6 +27,7 @@ public class ClientConfig
         this.keycloakAdminClient = keycloakClient;
     }
 
+    @Scheduled(initialDelay = 20000)
     public String getClientSecret()
     {
         if (clientSecret != null)
