49bc985976dc23cd437fb4b69a20ef9323b48937
Santiago Firpo <santiagoifirpo4@gmail.com>
2025-04-14 19:50:21 -0300

    Moved authorization controller to adapters/login

--- server/src/main/java/org/firpy/keycloakwrapper/adapters/login/AuthorizationController.java
+++ server/src/main/java/org/firpy/keycloakwrapper/adapters/login/AuthorizationController.java
@@ -1,4 +1,4 @@
-package org.firpy.keycloakwrapper.adapters;
+package org.firpy.keycloakwrapper.adapters.login;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
