fa7a6efc1b9014f780bfb990d7a4117bd610d15c
GuilhermeOchoa <guilhermeochoa@gmail.com>
2025-04-27 21:55:18 -0300

    Correções feitas em aula 23/04/25

--- src/main/java/com/constrsw/oauth/OauthApplication.java
+++ src/main/java/com/constrsw/oauth/OauthApplication.java
@@ -2,14 +2,20 @@ package com.constrsw.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(exclude = {
     DataSourceAutoConfiguration.class,
     DataSourceTransactionManagerAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
+    HibernateJpaAutoConfiguration.class,
+    MongoAutoConfiguration.class,
+    MongoDataAutoConfiguration.class,
+    MongoReactiveAutoConfiguration.class
 })
 public class OauthApplication {
     public static void main(String[] args) {
