5851fa08b4c0f67e200c8e23b94df1ed7fe23634
FuentesFilipe <fuentes.filipe@gmail.com>
2025-04-30 14:29:52 -0300

    refactor: remove unused CORS imports from SecurityConfig

--- src/main/java/com/grupo8/oauth/adapter/config/SecurityConfig.java
+++ src/main/java/com/grupo8/oauth/adapter/config/SecurityConfig.java
@@ -18,11 +18,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationConverter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
