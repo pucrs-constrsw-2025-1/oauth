447bdd8490168510f06975478fba3cf74c6c17bd
Guilherme Ochoa <guilhermeochoa@gmail.com>
2025-06-17 22:32:30 -0300

    correções de segurança para frontend

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 // src/app.ts
 
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import { authRouter } from './routes/auth.routes';
 import { usersRouter } from './routes/users.routes';
 import { rolesRouter } from './routes/roles.routes';
@@ -8,7 +10,35 @@ import { validateRouter } from './routes/validate.routes';
 import { setupSwagger } from './swagger';
 import { errorMiddleware } from './middlewares/error.middleware';
 
+dotenv.config();
+
 const app = express();
+
+// Configure CORS
+const corsOptions = {
+  origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+    const allowedOrigins = [
+      'http://localhost:8080', 
+      'http://127.0.0.1:8080', 
+      'http://localhost:3000', 
+      'http://127.0.0.1:3000'
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log('Blocked by CORS:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
